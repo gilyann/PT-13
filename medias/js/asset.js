ee03ddb437e3e3277cdf05ac939a1d8b877c7efa
Waffle-web <clairette02.martin@laposte.net>
2021-05-26 16:15:38 +0200

    ;;;

--- medias/js/asset.js
+++ medias/js/asset.js
@@ -1,5 +1,6 @@
 "use strict";
 
+<<<<<<< Updated upstream
 $(document).ready(function () {
 
    //gerer appartition du textes à revoir quand on hide le titre est pas tout le contenu et que la flèche n'affiche pas le contenu
@@ -53,3 +54,52 @@ $(document).ready(function () {
 // height: '+=10px',
 // right: '-=100px',
 // width: '+=2600px'
+=======
+$( document ).ready(function(){
+
+//gerer appartition du textes à revoir quand on hide le titre est pas tout le contenu et que la flèche n'affiche pas le contenu
+//$('.essai').hide();
+//$('.hublot2').on('click',function(){
+ // $('.essai').toggle();
+ 
+//})
+
+
+   //gerer l'apparition de la fiche 
+//$('.fiche').hide();                      
+//$('.essai').on('click',function(){
+ // $('.fiche').toggle(2000, "linear");
+  
+//})
+
+var numberimage=1;
+//les différents slides possibles 
+    $('.arrow-right').on('click',function(){            
+       $('.domaine-slide').animate({'left':(parseInt($('.domaine-slide').css('left'))-window.innerWidth)+'px'},function (){
+         numberimage++;
+        $('.paysage').attr('src','medias/img/bg'+ numberimage +'.jpg');   
+ 
+    })
+
+
+    $('.arrow-left').on('click',function(){            
+      $('.domaine-slide').animate({'left':(parseInt($('.domaine-slide').css('left'))+window.innerWidth)+'px'},function (){
+        numberimage--;
+       $('.paysage').attr('src','medias/img/bg' + numberimage +'.jpg');
+      })
+   })
+
+})
+})
+
+// //positioner l'image avec valeurs négative - animate 
+// $('.hublot .img-hublot').on('click',function(){
+//   $(this).animate({'margin-top':'-269px','width':'624px','height':'807px'})
+//   console.log('salut!');
+// })
+ 
+
+//    })
+
+   
+>>>>>>> Stashed changes
