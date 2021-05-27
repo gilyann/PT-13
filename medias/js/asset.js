62f188eaa95fdea2a6debe3df566f8a4925e1a00
Waffle-web <clairette02.martin@laposte.net>
2021-05-27 15:57:20 +0200

    changement hublot et vite fait formulaire

--- medias/js/asset.js
+++ medias/js/asset.js
@@ -1,8 +1,8 @@
 "use strict";
 
-<<<<<<< Updated upstream
 $(document).ready(function () {
-
+   $('.hublot').prepend('<div class="cache"></div>')
+   
    //gerer appartition du textes à revoir quand on hide le titre est pas tout le contenu et que la flèche n'affiche pas le contenu
    //$('.essai').hide();
    //$('.hublot2').on('click',function(){
@@ -38,13 +38,11 @@ $(document).ready(function () {
          $('.paysage').attr('src', 'medias/img/bg' + numberimage + '.jpg');
       })
    })
-   $('.cache1').on('click', function () {
-      $('.hublot').animate({
-         right: '+=1000px',
-         height: '-=1150px',
-         width: '+=150px'
-      });
 
+
+   $('.cache').on('click', function () {
+      $(this).parent().animate({'width': (parseInt($('.hublot').css('width')) + 6*WIDTH ) + 'px','margin-left':-1.6*WIDTH + 'px'});
+      
       console.log("hello")
    })
 })
@@ -54,52 +52,6 @@ $(document).ready(function () {
 // height: '+=10px',
 // right: '-=100px',
 // width: '+=2600px'
-=======
-$( document ).ready(function(){
-
-//gerer appartition du textes à revoir quand on hide le titre est pas tout le contenu et que la flèche n'affiche pas le contenu
-//$('.essai').hide();
-//$('.hublot2').on('click',function(){
- // $('.essai').toggle();
- 
-//})
-
-
-   //gerer l'apparition de la fiche 
-//$('.fiche').hide();                      
-//$('.essai').on('click',function(){
- // $('.fiche').toggle(2000, "linear");
-  
-//})
-
-var numberimage=1;
-//les différents slides possibles 
-    $('.arrow-right').on('click',function(){            
-       $('.domaine-slide').animate({'left':(parseInt($('.domaine-slide').css('left'))-window.innerWidth)+'px'},function (){
-         numberimage++;
-        $('.paysage').attr('src','medias/img/bg'+ numberimage +'.jpg');   
- 
-    })
-
-
-    $('.arrow-left').on('click',function(){            
-      $('.domaine-slide').animate({'left':(parseInt($('.domaine-slide').css('left'))+window.innerWidth)+'px'},function (){
-        numberimage--;
-       $('.paysage').attr('src','medias/img/bg' + numberimage +'.jpg');
-      })
-   })
-
-})
-})
-
-// //positioner l'image avec valeurs négative - animate 
-// $('.hublot .img-hublot').on('click',function(){
-//   $(this).animate({'margin-top':'-269px','width':'624px','height':'807px'})
-//   console.log('salut!');
-// })
- 
-
-//    })
-
-   
->>>>>>> Stashed changes
+// right: '+=1000px',
+// height: '-=1150px',
+// width: '150px'
