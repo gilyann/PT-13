743d79d7bb1e90efc5dfe765d7f8c950ce872d2c
Waffle-web <clairette02.martin@laposte.net>
2021-05-31 19:15:50 +0200

    ?

--- domaine.php
+++ domaine.php
@@ -199,55 +199,7 @@ echo('</section>');
 
 
     <section class="tdf masteres">
-        <div class="intitule">
-            <h2>Mastères</h2>
-            <ul>
-                <li cat="4" lien="1">Intitulé 1</li>
-                <li cat="4" lien="2">Intitulé 2</li>
-                <li cat="4" lien="3">Intitulé 3</li>
-                <li cat="4" lien="4">Intitulé 4</li>
-                <li cat="4" lien="5">Intitulé 5</li>
-            </ul>
-            <a href="#" class="lien">Voir toutes les formations</a>
-        </div>
-        <?php
-
-$link = new PDO('mysql:host=localhost;dbname=mmi_airline', 'root', '', array
-(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-// pour le serveur de l'UPEM, remplacer localhost par sqletud.u-pem.fr
-$sql = "SELECT nom_formation, enseignements, alternance_initiale, debouches, nom_etablissement, ville, code_postal, url_formation, intitule_formation.id_intitule
-FROM  intitule_formation, rel_domaine_int_form 
-WHERE id_domaine =3
-AND rel_domaine_int_form.id_intitule=intitule_formation.id_intitule 
-AND id_formation_type=5"; 
-// On prépare la requête avant l'envoi :
-$req = $link -> prepare($sql);
-$req -> execute();
-$i=0;
-// On crée une liste non numérotée avec les résultats
-while($data = $req -> fetch()){
- // On affiche chaque résultat sous forme d'un item de la liste
- echo '<main class="card cat3 c'.$data['id_intitule'].' '.$i.'">
- <div class="exit"></div>
- <h3>'.$data['nom_formation'].'</h3>
- <p class="truc">Enseignements</p>
- <p>'.$data['enseignements'].'</p>
- <p class="type">Type</p>
- <p>'.$data['alternance_initiale'].'</p>
- <p class="deb">Débouchés</p>
- <p>'.$data['debouches']. '</p>
- <p class="eta">Etablissements</p>
- <p>'.$data['nom_etablissement'].' - '.$data['ville'].' ('.$data['code_postal'].')</p> 
- <p class="url">URL</p>
- <p>'.$data['url_formation'].'</p>
-
-<div class="timbre"></div>
-</main>';
-$i++;
-}
-$req = null;
-echo('</section>'); 
-?>
+</section>
   
 </body>
 
