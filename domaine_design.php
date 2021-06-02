2e49db2b281642ca48b0885fd2a5bc5d857a0e56
Waffle-web <clairette02.martin@laposte.net>
2021-06-02 21:13:21 +0200

    dernière page faites
    y reste des trucs à faire mais ça avance

--- domaine_design.php
+++ domaine_design.php
@@ -11,7 +11,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="medias/js/domaine_audiovisuel.js"></script>
+    <script src="medias/js/domaine_design.js"></script>
     <title>domaine_audiovisuel</title>
 </head>
 
@@ -28,8 +28,8 @@
     </nav>
 
     <header>
-        <p>Bienvenue dans le domaine de</p>
-        <h1>L'AUDIOVISUEL</h1>
+        <p>Bienvenue dans le domaine du</p>
+        <h1>DESIGN</h1>
         <p>En visitant les différentes attractions, tu découvriras les différents types d'études possibles !</p>
     </header>
 
@@ -37,12 +37,9 @@
         <div class="intitule">
             <h2>Licences pros</h2>
             <ul>
-                <li cat="1" lien="0">Cinéma et Audiovisuel</li> 
-                <li cat="1" lien="1">Cinéma</li>
-                <li cat="1" lien="2">Gestion de la production audiovisuelle</li>
-                <li cat="1" lien="3">Techniques et activités de l'image et du son</li>
-                <li cat="1" lien="4">Techniques du son et de l'image parcours Métiers du Motion et du sound design
-                </li>
+                <li cat="1" lien="0">Infographie,webdesign et multimédia</li> 
+                <li cat="1" lien="1">Métiers du jeu vidéo</li>
+                <li cat="1" lien="3">Techniques de l'image et du son spécialité Infographie et webdesign</li>
             </ul>
             <a href="#" class="lien">Voir toutes les formations</a>
         </div> 
@@ -55,7 +52,7 @@ $link = new PDO('mysql:host=localhost;dbname=mmi-airline', 'root', '', array
 // pour le serveur de l'UPEM, remplacer localhost par sqletud.u-pem.fr
 $sql = "SELECT nom_formation, enseignements, alternance_initiale, debouches, nom_etablissement, ville, code_postal, url_formation, intitule_formation.id_intitule
 FROM  intitule_formation, rel_domaine_int_form 
-WHERE id_domaine =3
+WHERE id_domaine =4
 AND rel_domaine_int_form.id_intitule=intitule_formation.id_intitule 
 AND id_formation_type=1"; 
 // On prépare la requête avant l'envoi :
@@ -94,11 +91,11 @@ echo('</section>');
         <div class="intitule">
             <h2>Bachelors</h2>
             <ul>
-                <li cat="2"lien="0">Monteur</li>
-                <li cat="2" lien="1">Assistant de production audiovisuelle</li> 
-                <li cat="2" lien="2">Film making</li>
-                <li cat="2" lien="3">Chef opérateur son</li>
-                <li cat="2" lien="4">Cinéma Animation 3D</li>
+                <li cat="2"lien="0">Animateur 2D</li>
+                <li cat="2" lien="1">Animateur 3D</li> 
+                <li cat="2" lien="2">Animation & réalisation 3D</li>
+                <li cat="2" lien="5">Création et design</li>
+                <li cat="2" lien="4">Concepteur Jeu vidéo</li>
             </ul>
             <a href="#" class="lien">Voir toutes les formations</a>
         </div> 
@@ -108,7 +105,7 @@ echo('</section>');
   (PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
   $sql = "SELECT nom_formation, enseignements, alternance_initiale, debouches, nom_etablissement, ville, code_postal, url_formation, intitule_formation.id_intitule
   FROM  intitule_formation, rel_domaine_int_form 
-  WHERE id_domaine =3
+  WHERE id_domaine =4
   AND rel_domaine_int_form.id_intitule=intitule_formation.id_intitule 
   AND id_formation_type=2"; 
  // On prépare la requête avant l'envoi :
@@ -149,11 +146,10 @@ echo('</section>');
         <div class="intitule">
             <h2>Masters</h2>
             <ul>
-                <li cat="3"lien="0">Acoustique</li>
-                <li cat="3"lien="1">Audiovisuel, médias interactifs numériques, jeux</li>
-                <li cat="3"lien="2">Cinéma et audiovisuel</li>
-                <li cat="3"lien="3">Réalisation et création</li>
-                <li cat="3"lien="4">Cinéma et audiovisuel</li>
+                <li cat="3"lien="0">Design</li>
+                <li cat="3"lien="1">Optique, image, vision, multimédia parcours 3D multimédia</li>
+                <li cat="3"lien="2">Optique, image, vision, multimédia parcours systèmes distribués et technologies de la <br>
+                    Data Science</li>
             </ul>
             <a href="#" class="lien">Voir toutes les formations</a>
         </div>
@@ -167,7 +163,7 @@ $link = new PDO('mysql:host=localhost;dbname=mmi-airline', 'root', '', array
 // pour le serveur de l'UPEM, remplacer localhost par sqletud.u-pem.fr
 $sql = "SELECT nom_formation, enseignements, alternance_initiale, debouches, nom_etablissement, ville, code_postal, url_formation, intitule_formation.id_intitule
 FROM  intitule_formation, rel_domaine_int_form 
-WHERE id_domaine =3
+WHERE id_domaine =4
 AND rel_domaine_int_form.id_intitule=intitule_formation.id_intitule 
 AND id_formation_type=4"; 
 // On prépare la requête avant l'envoi :
@@ -199,7 +195,109 @@ $req = null;
 echo('</section>'); 
 ?>
 
+<section class="tdf masters">
+        <div class="intitule">
+            <h2>Cycles d'ingénieur</h2>
+            <ul>
+                <li cat="3"lien="0">Formation IMAC</li>
+                <li cat="3"lien="1">Programme Grande École</li>
+            </ul>
+            <a href="#" class="lien">Voir toutes les formations</a>
+        </div>
 
+
+
+        <?php
+$link = new PDO('mysql:host=localhost;dbname=mmi-airline', 'root', '', array
+(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+// pour le serveur de l'UPEM, remplacer localhost par sqletud.u-pem.fr
+$sql = "SELECT nom_formation, enseignements, alternance_initiale, debouches, nom_etablissement, ville, code_postal, url_formation, intitule_formation.id_intitule
+FROM  intitule_formation, rel_domaine_int_form 
+WHERE id_domaine =4
+AND rel_domaine_int_form.id_intitule=intitule_formation.id_intitule 
+AND id_formation_type=3"; 
+// On prépare la requête avant l'envoi :
+$req = $link -> prepare($sql);
+$req -> execute();
+$i=0;
+// On crée une liste non numérotée avec les résultats
+while($data = $req -> fetch()){
+ // On affiche chaque résultat sous forme d'un item de la liste
+ echo '<main class="card cat3 c'.$data['id_intitule'].' '.$i.'">
+ <div class="exit"></div>
+ <h3>'.$data['nom_formation'].'</h3>
+ <p class="truc">Enseignements</p>
+ <p>'.$data['enseignements'].'</p>
+ <p class="type">Type</p>
+ <p>'.$data['alternance_initiale'].'</p>
+ <p class="deb">Débouchés</p>
+ <p>'.$data['debouches']. '</p>
+ <p class="eta">Etablissements</p>
+ <p>'.$data['nom_etablissement'].' - '.$data['ville'].' ('.$data['code_postal'].')</p> 
+ <p class="url">URL</p>
+ <p>'.$data['url_formation'].'</p>
+
+<div class="timbre"></div>
+</main>';
+$i++;
+}
+$req = null;
+echo('</section>'); 
+?>
+
+
+<section class="tdf masters">
+        <div class="intitule">
+            <h2>Mastères</h2>
+            <ul>
+                <li cat="3"lien="0">Game Art</li>
+                <li cat="3"lien="1">Jeu Vidéo</li>
+                <li cat="3"lien="2">Game design</li>
+                <li cat="3"lien="3">Interactivité et UX design</li>
+                <li cat="3"lien="4">Marketing Digital & UX</li>
+            </ul>
+            <a href="#" class="lien">Voir toutes les formations</a>
+        </div>
+
+
+        <?php
+
+$link = new PDO('mysql:host=localhost;dbname=mmi-airline', 'root', '', array
+(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+// pour le serveur de l'UPEM, remplacer localhost par sqletud.u-pem.fr
+$sql = "SELECT nom_formation, enseignements, alternance_initiale, debouches, nom_etablissement, ville, code_postal, url_formation, intitule_formation.id_intitule
+FROM  intitule_formation, rel_domaine_int_form 
+WHERE id_domaine =4
+AND rel_domaine_int_form.id_intitule=intitule_formation.id_intitule 
+AND id_formation_type=5"; 
+// On prépare la requête avant l'envoi :
+$req = $link -> prepare($sql);
+$req -> execute();
+$i=0;
+// On crée une liste non numérotée avec les résultats
+while($data = $req -> fetch()){
+ // On affiche chaque résultat sous forme d'un item de la liste
+ echo '<main class="card cat3 c'.$data['id_intitule'].' '.$i.'">
+ <div class="exit"></div>
+ <h3>'.$data['nom_formation'].'</h3>
+ <p class="truc">Enseignements</p>
+ <p>'.$data['enseignements'].'</p>
+ <p class="type">Type</p>
+ <p>'.$data['alternance_initiale'].'</p>
+ <p class="deb">Débouchés</p>
+ <p>'.$data['debouches']. '</p>
+ <p class="eta">Etablissements</p>
+ <p>'.$data['nom_etablissement'].' - '.$data['ville'].' ('.$data['code_postal'].')</p> 
+ <p class="url">URL</p>
+ <p>'.$data['url_formation'].'</p>
+
+<div class="timbre"></div>
+</main>';
+$i++;
+}
+$req = null;
+echo('</section>'); 
+?>
 
 
     <section class="tdf masteres">
