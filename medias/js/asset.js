7cc11a82c4d9af4cda3765166f95294548e0ec49
Waffle-web <clairette02.martin@laposte.net>
2021-06-05 20:11:05 +0200

    enlever des trucs

--- medias/js/asset.js
+++ medias/js/asset.js
@@ -3,19 +3,6 @@
 $(document).ready(function () {
    $('.hublot').prepend('<div class="cache"></div>')
    
-   //gerer appartition du textes à revoir quand on hide le titre est pas tout le contenu et que la flèche n'affiche pas le contenu
-   //$('.essai').hide();
-   //$('.hublot2').on('click',function(){
-   // $('.essai').toggle();
-
-   //})
-
-   //gerer l'apparition de la fiche 
-   //$('.fiche').hide();                      
-   //$('.essai').on('click',function(){
-   // $('.fiche').toggle(2000, "linear");
-
-   //})
    var WIDTH = window.innerWidth;
 
    var numberimage = 1;
@@ -49,12 +36,3 @@ $(document).ready(function () {
       console.log("hello")
    })
 })
-
-
-
-// height: '+=10px',
-// right: '-=100px',
-// width: '+=2600px'
-// right: '+=1000px',
-// height: '-=1150px',
-// width: '150px'
