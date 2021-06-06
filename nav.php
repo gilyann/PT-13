0d6070a2440f2adc6810857de0f03f8cad7d3e40
gilyann <gilyann.padre@gmail.com>
2021-06-06 18:56:41 +0200

    re

--- nav.php
+++ nav.php
@@ -2,7 +2,7 @@
 echo("<nav>
 <a href='index.php' class='logo'>MMi Airlines</a>
 <ul>
-<li> <a href='hublot.html'>Poursuite d'études</a>  </li>
+<li> <a href='hublot.php'>Poursuite d'études</a>  </li>
 <li><a href='BUT.html'>BUT MMI</a></li>
 <li><a href='a_propos.html'>L'équipage</a></li>
 </ul>
