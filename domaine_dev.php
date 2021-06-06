ba5625fa15829349f775267480a658a0d928ce30
gilyann <gilyann.padre@gmail.com>
2021-06-06 19:32:43 +0200

    nav dev

--- domaine_dev.php
+++ domaine_dev.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="domaine_dev.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="nav.css">
+<link rel="stylesheet" href="domaine_dev.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="medias/js/domaine.js"></script>
     <title>domaine_développement</title>
--- medias/js/domaine.js
+++ medias/js/domaine.js
@@ -7,16 +7,34 @@ $(document).ready(function () {
 
 
         $(".intitule li").on("click", function () {
-var lien =$(this).attr('data-lien')
-var cat =$(this).attr('data-cat')
+            var lien = $(this).attr('data-lien')
+            var cat = $(this).attr('data-cat')
 
-$('main.cat'+cat+'').removeClass('active')
-            $('main.cat'+cat+'.lien'+lien).addClass("active") 
+            $('main.cat' + cat + '').removeClass('active')
+            $('main.cat' + cat + '.lien' + lien).addClass("active")
         })
         $(".exit").on("click", function () {
 
-            $(this).parent().removeClass('active') 
+            $(this).parent().removeClass('active')
 
         })
     })
+
+    window.addEventListener('scroll', function (event) {
+
+        if (window.scrollY <= 200) {
+            $('nav').css({
+                'backgroundColor': 'transparent',
+                'box-shadow': 'none'
+            })
+            $('nav a').css('color', 'white')
+
+        } else {
+            $('nav').css({
+                'backgroundColor': '#2fa0d16c',
+                'box-shadow': '1px 1px 12px #2fa0d16c'
+            })
+            $('nav a').css('color', 'white')
+        }
     })
+})
