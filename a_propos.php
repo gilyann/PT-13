e416052746e600e61ff462cfe62c5dd3549d2251
Lucas <lucasp0706@gmail.com>
2021-06-06 02:29:55 +0200

    a_propos
    mise en page (perso, cercle, barre beige)

--- a_propos.php
+++ a_propos.php
@@ -8,86 +8,98 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="a_propos.css">
-    <title>A propos</title>
+    <link rel="stylesheet" href="nav.css">
+    <title>L'équipage</title>
 </head>
 
 <body>
 
-    <nav>
-        <div class="left">
-            <img src="medias/img/LOGOBLEU.svg" alt=" " class="logo">
-        </div>
-        <div class="right">
-            <a href="">Poursuite d'étude</a>
-            <a href="">BUT</a>
-            <a href="">A propos</a>
-        </div>
-    </nav>
+<?php include 'nav.php' ?>
 
     <section class="personnage">
         <div class="chara gilyann">
             <div class="perso">
                 <img src="medias/img/Gilyann.png" alt="">
-                <h2>Gilyann Padre : graphiste et developpeur full-stack</h2>
+                <div class="beige"></div>
+                <h2><span>Gilyann Padre</span><br> Graphiste <br> et developpeur full-stack</h2>
             </div>
             <div class="lien">
+                <div class="rond">
                 <a href="medias/cv/CV_Gilyann_Padre.pdf" class="cv">
                     <img src="medias/img/paper_cv.svg" alt="cv pdf Gilyann Padre">
                 </a>
-                <a href="#" class="interactif">
-                    <img src="medias/img/desktop_1.svg" alt="cv intéractif Gilyann Padre">
+            </div>
+            <div class="rond">
+                <a href="https://etudiant.u-pem.fr/~gpadre/Cv_interactif/" class="interactif">
+                    <img src="medias/img/desktop_1.svg" alt="cv interactif Gilyann Padre">
                 </a>
+                </div>
             </div>
         </div>
 
         <div class="chara Claire">
             <div class="perso">
                 <img src="medias/img/Claire.png" alt="">
-                <h2>Claire Martin : développeuse <br> full-stack</h2>
+                <div class="beige"></div>
+                <h2><span>Claire Martin</span> <br> Développeuse <br>full-stack</h2>
             </div>
             <div class="lien">
+                <div class="rond">
                 <a href="medias/cv/CV_Claire_Martin.pdf" class="cv">
                     <img src="medias/img/paper_cv.svg" alt="cv pdf Claire Martin">
                 </a>
-                <a href="#" class="interactif">
-                    <img src="medias/img/desktop_1.svg" alt="cv intéractif Claire Martin">
+            </div>
+            <div class="rond">
+                <a href="https://etudiant.u-pem.fr/~cmarti60/cv/">
+                    <img src="medias/img/desktop_1.svg" alt="cv interactif Claire Martin">
                 </a>
+            </div>
             </div>
         </div>
 
         <div class="chara Lucas">
-            <div class="perso">
+            <div class="perso lucas">
                 <img src="medias/img/Lucas.png" alt="">
-                <h2>Lucas Pham : Chargé de communication écrite et visuelle</h2>
+                <div class="beige"></div>
+                <h2><span>Lucas Pham</span> <br>Chargé de communication <br> écrite et visuelle</h2>
             </div>
             <div class="lien">
+                <div class="rond">
                 <a href="medias/cv/cv_Lucas_Pham.pdf" class="cv">
                     <img src="medias/img/paper_cv.svg" alt="cv pdf Lucas Pham">
                 </a>
-                <a href="#" class="interactif">
-                    <img src="medias/img/desktop_1.svg" alt="cv intéractif Lucas Pham">
+            </div>
+            <div class="rond">
+                <a href="https://etudiant.u-pem.fr/~lpham03/cv-interactif-lucas-pham/">
+                    <img src="medias/img/desktop_1.svg" alt="cv interactif Lucas Pham">
                 </a>
             </div>
         </div>
+        </div>
 
-        <div class="chara Julianne">
-            <div class="perso">
+        <div class="chara">
+            <div class="perso julianne">
                 <img src="medias/img/Julianne.png" alt="">
-                <h2>Julianne Vial : Cheffe de projet et chargé de communication</h2>
             </div>
+            <div class="beige"></div>
+                <h2><span>Julianne Vial</span> <br> Cheffe de projet et chargé de communication</h2>
             <div class="lien">
+                <div class="rond">
                 <a href="medias/cv/CV-Julianne_Vial.pdf" class="cv">
                     <img src="medias/img/paper_cv.svg" alt="cv pdf Julianne Vial">
                 </a>
+            </div>
+            <div class="rond">
                 <a href="#" class="interactif">
-                    <img src="medias/img/desktop_1.svg" alt="cv intéractif Julianne Vial">
+                    <img src="medias/img/desktop_1.svg" alt="cv interactif Julianne Vial">
                 </a>
+            </div>
             </div>
         </div>
     </section>
 
     <section class="remerciement">
-        <p> Mention 1jour1ecole sur le site : <br>
+        <p> Remerciements : <br>
             MMi Airlines est en collaboration avec le compte Instagram 1jour1école. Celui-ci compte plus d’une vingtaine
             de témoignages d'étudiants issus d’un DUT MMI expliquant leur poursuite d’études de manière détaillée et
             précise. Tu peux en rencontrer lors de ton voyage alors ouvre bien les yeux !
